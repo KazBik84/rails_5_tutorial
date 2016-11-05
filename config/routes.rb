09914dab3317ea55b862e5c64eecc7869083040d
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-11-05 00:44:25 -0300

    Generated Sessions controller with new template

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+
   get 'users/new'
 
   get 'user/new'
 
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
-  root 'static_pages#home'  
-  get '/help'  , to: 'static_pages#help'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
   get '/home', to: 'static_pages#home'
 
   get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 
