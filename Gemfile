b1f2c583497da794e0a78cf44c1a26141567cb93
kazbik84 <kazimierz.bik@gmail.com>
2016-11-22 22:21:32 +0100

    Added micropost destroy tests and gems for image upload

--- Gemfile
+++ Gemfile
@@ -14,6 +14,10 @@ gem 'haml'
 gem 'faker'
 gem 'will_paginate',           '3.1.0'
 gem 'bootstrap-will_paginate', '0.0.10'
+gem 'carrierwave',             '0.11.2'
+gem 'mini_magick',             '4.5.1'
+gem 'fog',                     '1.38.0'
+
 
 
 group :development, :test do
---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'micropost inteface' do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    #Invalid submission
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: '' } }
+    end
+    assert_select 'div#error_explanation'
+    # Valid sumission
+    content = 'This micropost ties the room together'
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost: { content: content } }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+    # Delete body
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    # Visit different user (no delete links)
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
+  end
+end
