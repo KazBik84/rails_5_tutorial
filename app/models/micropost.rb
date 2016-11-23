ba0fc9f86ad979736ff3ef5f8bd5c3233efae12f
kazbik84 <kazimierz.bik@gmail.com>
2016-11-23 21:24:23 +0100

    Add user microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,15 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  # Custom validations are called valdate as oposite, 
+  # to predefined validates
+  validate :picture_size
+
+
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.size(:picture, 'should be less than 5MB')
+      end
+    end
 end
